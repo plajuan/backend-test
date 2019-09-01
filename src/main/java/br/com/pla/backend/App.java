7516d0962e66d6ede107343a3fd4f124fdbaff80
plajuan <pla.juan@gmail.com>
2019-08-31 21:17:13 -0300

    Main app and pom file

---
+++ src/main/java/br/com/pla/backend/App.java
@@ -0,0 +1,16 @@
+package br.com.pla.backend;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Read from log file
+		
+		//TODO Read from https://my-json-server.typicode.com/cairano/backend-test/pagamentos
+		
+		//TODO Read from https://my-json-server.typicode.com/cairano/backend-test/recebimentos
+		
+		//TODO Read from https://my-json-server.typicode.com/cairano/backend-test/db
+
+	}
+
+}
